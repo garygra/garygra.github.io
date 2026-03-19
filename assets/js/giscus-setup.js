e9f223b44cec45b7af5c5386315ff4cb1f34aee3
garygra <gary.granados@gmail.com>
2026-03-19 20:05:21 +0000

    Deploying to gh-pages from @ garygra/garygra@5f28cc54db75fa94bd2a0a82b23426ec82e7c29b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "garygra/personal_website",
+    "data-repo": "garygra/garygra",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
